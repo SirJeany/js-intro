f219e00484d8a5ee9e89da6360c95ca9a692fffd
SirJeany <jmvanriet1@gmail.com>
2020-03-10 10:38:31 +0200

    Train

--- js/loops.js
+++ js/loops.js
@@ -1,23 +1,34 @@
 'use strict';
 // While loop implementation:
-console.log("------START------");
+// console.log("------START------");
 
-let num = 0;
-while(num <= 10) {
-    console.log(num);
-    num = num + 2;
-}
+// let num = 0;
+// while(num <= 10) {
+//     console.log(num);
+//     num = num + 2;
+// }
 
-console.log("------END-------")
+// console.log("------END-------")
 
 // for loop (note that the loops seem similar at first, but output is different)
-console.log("------START------");
+// console.log("------START------");
 
-num = 0;
-for(let i = 0; i <= 10; i++) {
-    console.log(num);
-    num = num + 2;
+// num = 0;
+// for(let i = 0; i <= 10; i++) {
+//     console.log(num);
+//     num = num + 2;
+// }
+
+// console.log("------END-------")
+
+// Ten passangers to board a train. Not concerned with the order of boarding.. Use a for loop to board them, one-by-one until we get to ten.
+
+console.log("------START TRAIN------");
+
+let passNum = 1;
+for(let i = 1; i <= 10; i++) {
+    console.log("Hi, welcome to the train, passanger number " + passNum);
+    passNum++;
 }
 
-console.log("------END-------")
-
+console.log("------END TRAIN-------")
