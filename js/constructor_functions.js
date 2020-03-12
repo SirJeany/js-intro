1629b72250c7f4a41ce66b30c5093cba912c4d0a
SirJeany <jmvanriet1@gmail.com>
2020-03-12 11:22:08 +0200

    Making our own furniture using ctr funcs

--- js/constructor_functions.js
+++ js/constructor_functions.js
@@ -1,5 +1,5 @@
 'use strcit';
-
+/*
 function Pastry(type, flavour, layers, price, occasion) {
     this.type = type;
     this.flavour = flavour;
@@ -22,3 +22,35 @@ console.log(dnut.describe());
 
 let eclaire = new Pastry('Eclaire', 'Strawberry');
 console.log(eclaire.bake());
+*/
+// Create a furniture shop with 5 different products.
+
+function Furniture(type, color, price, materials, img) {
+    this.type = type;
+    this.color = color;
+    this.price = price;
+    this.materials = materials;
+    this.img = img;
+}
+
+Furniture.prototype.describe = function() {
+    return "This is a designer " + this.type + "with: \n" + "Colour: " + this.color + "\n" + "Price: " + this.price + "\n" + "Matrials: " + this.materials + "\n" + "img: " + this.img + "\n";
+}
+
+Furniture.prototype.build = function() {
+    return "Building a " + this.type;
+}
+
+
+//TEST
+
+var anObject = {
+    a:2
+}
+
+document.getElementById("showBtn").addEventListener("click", print(anObject));
+
+
+function print(obj) {
+    document.getElementById("output").innerHTML = obj.a;
+}
