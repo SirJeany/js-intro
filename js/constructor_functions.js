ac93fe82ec953d6a75b7ff1487c5e7d82ac8b68a
SirJeany <jmvanriet1@gmail.com>
2020-03-12 11:40:52 +0200

    Added new furniture shop

--- js/constructor_functions.js
+++ js/constructor_functions.js
@@ -34,23 +34,52 @@ function Furniture(type, color, price, materials, img) {
 }
 
 Furniture.prototype.describe = function() {
-    return "This is a designer " + this.type + "with: \n" + "Colour: " + this.color + "\n" + "Price: " + this.price + "\n" + "Matrials: " + this.materials + "\n" + "img: " + this.img + "\n";
+    return "This is a designer " + this.type + " that is: \n" + "Colour: " + this.color + "\n" + "Price: R" + this.price + ".00\n" + "Matrials used: " + this.materials + "\n" + "img: " + this.img + "\n";
 }
 
 Furniture.prototype.build = function() {
     return "Building a " + this.type;
 }
 
+// High Chair:
+let highChair = new Furniture('High chair', 'Black', 3000, ['Steel', 'Wood'], 'path/to/img');
+
+console.log(highChair.describe());
+
+// Marble Table:
+let marbleTable = new Furniture('Marble Table', 'White and grey', 10000, ['Marble', 'Wood'], 'path/to/img');
+
+console.log(marbleTable.describe());
+
+// Crazy Couch:
+let crazyCouch = new Furniture('Crazy couch', 'Black', 3000, ['Leather', 'Wood'], 'path/to/img');
+
+console.log(crazyCouch.describe());
+
+
+// Squazy TV stand:
+let squazyTV = new Furniture('Squazy TV stand', 'Red', 3000, ['Wood', 'Glass', 'Steel'], 'path/to/img');
+
+console.log(squazyTV.build());
+
+
+// Outside Chair:
+let outsideChair = new Furniture('Outside Chair', 'White', 3000, ['Stainless Steel'], 'path/to/img');
+
+console.log(outsideChair.describe());
+
+
+
 
 //TEST
 
-var anObject = {
-    a:2
-}
+// var anObject = {
+//     a:2
+// }
 
-document.getElementById("showBtn").addEventListener("click", print(anObject));
+// document.getElementById("showBtn").addEventListener("click", print(anObject));
 
 
-function print(obj) {
-    document.getElementById("output").innerHTML = obj.a;
-}
+// function print(obj) {
+//     document.getElementById("output").innerHTML = obj.a;
+// }
